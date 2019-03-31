e47a7a1ae8311c0adc2b23efd5f7bf4af4a92aa8
“masudulhas” <masudulhas@gmail.com>
2019-03-31 01:33:41 +0100

    weather first commit

---
+++ src/app/index.js
@@ -0,0 +1,42 @@
+/* global google */
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Autocomplete from 'react-google-autocomplete';
+/* global google */
+
+class App extends Component {
+  state = {
+    weather: {}
+  };
+
+  
+  render() {
+
+     return (
+      <Autocomplete
+      style={{width: '90%'}}
+      onPlaceSelected={(place) => {
+        let latitute = place.geometry.location.lat();
+        let lat = parseFloat(latitute).toFixed(2);
+        let longitude = place.geometry.location.lng();
+        let lng = parseFloat(longitude).toFixed(2);
+        let apiReq = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+lng+"/lat/"+lat+"/data.json";
+        
+        fetch(apiReq).then(response => response.json()).then(
+          data => {
+            this.setState({weather: data.timeSeries})
+          }
+        );
+      
+
+      }}
+
+      types={['(regions)']}
+      componentRestrictions={{country: "SE"}}
+  />
+      
+    );
+  }
+}
+
+render(<App />, document.getElementById('root'));
