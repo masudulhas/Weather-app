15b455e993e7f24acccad81f525622ad99a7daff
“masudulhas” <masudulhas@gmail.com>
2019-03-31 14:37:35 +0200

    Add weather output

---
+++ src/app/components/WeatherOutput.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const WeatherOutput = (props) =>{
+
+    return(
+        <div>
+            < table>
+                <thead>
+                    <tr>
+                        <th>ReferenceTime</th>
+                        <th>Parameter</th>
+                        <th>Level</th>
+                        <th>Lan</th>
+                        <th>Lat</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    < tr > 
+                        <td>{props.weather.toString()}</td>
+                         <td>{props.name}</td>
+                         <td>{props.level}</td>
+                         <td>{props.lan}</td>
+                         <td>{props.lat}</td>
+                    </tr >
+                </tbody>
+            </table>
+        </div>
+    );
+
+}
+export default WeatherOutput
--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Autocomplete from 'react-google-autocomplete';
+import WeatherOutput from './components/WeatherOutput'
 /* global google */
 
 class App extends Component {
@@ -10,32 +11,42 @@ class App extends Component {
   };
 
 render() {
-
      return (
-      <Autocomplete
-      style={{width: '90%'}}
-      onPlaceSelected={(place) => {
-        let latitute = place.geometry.location.lat();
-        let lat = parseFloat(latitute).toFixed(2);
-        let longitude = place.geometry.location.lng();
-        let lng = parseFloat(longitude).toFixed(2);
-        let apiReq = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+lng+"/lat/"+lat+"/data.json";
-        
+       < div className = "container" >
+        <Autocomplete
+          style={{width: '50%'}}
+          onPlaceSelected={(place) => {
+            let latitute = place.geometry.location.lat();
+            let lat = parseFloat(latitute).toFixed(2);
+            let longitude = place.geometry.location.lng();
+            let lng = parseFloat(longitude).toFixed(2);
+            let apiReq = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/"+lng+"/lat/"+lat+"/data.json";
+          
         fetch(apiReq).then(response => response.json()).then(
-          data => {
-            this.setState({weather: data.timeSeries})
+          data => { this.setState({
+              weather: data.timeSeries[0].validTime,
+              name: data.timeSeries[0].parameters[0].name,
+              level: data.timeSeries[0].parameters[0].level,
+              unit: data.timeSeries[0].parameters[0].unit,
+              lan: data.geometry.coordinates[0][0],
+              lat: data.geometry.coordinates[0][1]
+            })
           }
         );
-      
-
       }}
-
       types={['(regions)']}
       componentRestrictions={{country: "SE"}}
-  />
-      
+    />
+    
+    < WeatherOutput  
+        weather = {this.state.weather}
+        name = {this.state.name}
+        level = {this.state.level}
+        lan = {parseFloat(this.state.lan).toFixed(2)}
+        lat = {parseFloat(this.state.lat).toFixed(2)}
+    />
+   </div>   
     );
   }
 }
-
 render(<App />, document.getElementById('root'));
